44440d3560ced5a25290dc8e73e1d915644aeeac
borgai74-create <borgai74@gmail.com>
2026-04-07 13:07:12 +0200

    Fix: Remove incorrect SQLAlchemy import

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, Column, Integer, String, Date, Text, ForeignKey, TIMESTAMP, Enum, Numeric, JSON
+from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey, TIMESTAMP, Enum, Numeric, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 import datetime
